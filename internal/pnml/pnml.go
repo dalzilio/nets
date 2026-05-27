97a99fe8a8c0f0a337ed6458c2cd6597aef3b365
Silvano DAL ZILIO <2209941+dalzilio@users.noreply.github.com>
2026-05-27 10:08:02 +0200

    harmonized LICENCE for. internal package

--- internal/pnml/pnml.go
+++ internal/pnml/pnml.go
@@ -1,6 +1,6 @@
-// Copyright (c) 2021 Silvano DAL ZILIO
-//
-// GNU Affero GPL v3
+// Copyright 2026. Silvano DAL ZILIO. All rights reserved.
+// Use of this source code is governed by the AGPL license
+// that can be found in the LICENSE file.
 
 package pnml
 
